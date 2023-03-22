ce6c62cee2d39d6091a75c9b2595d96cb77f4b7d
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-03-22 15:02:47 +0900

    아이템정보 표시

---
+++ experiment/loa.js
@@ -0,0 +1,166 @@
+data = {
+    'Type': '무기',
+    'Name': '+20 악몽의 꽃 마법 덱',
+    'Icon': 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/MAR_Item/MAR_Item_01_42.png',
+    'Grade': '유물',
+    'Tooltip': {
+        "Element_000": {
+            "type": "NameTagBox",
+            "value": "<P ALIGN=\'CENTER\'><FONT COLOR=\'#FA5D00\'>+20 악몽의 꽃 마법 덱</FONT></P>"
+        },
+        "Element_001": {
+            "type": "ItemTitle",
+            "value": {
+                "leftStr0": "<FONT SIZE=\'12\'><FONT COLOR=\'#FA5D00\'>유물 타로카드</FONT></FONT>",
+                "leftStr1": "<FONT SIZE=\'14\'>품질</FONT>",
+                "leftStr2": "<FONT SIZE=\'14\'>아이템 레벨 1490 (티어 3)</FONT>",
+                "qualityValue": 58,
+                "rightStr0": "<FONT SIZE=\'12\'><FONT COLOR=\'#FFD200\'>장착중</FONT></FONT>",
+                "slotData": {
+                    "advBookIcon": 0,
+                    "battleItemTypeIcon": 0,
+                    "cardIcon": false,
+                    "friendship": 0,
+                    "iconGrade": 5,
+                    "iconPath": "https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/MAR_Item/MAR_Item_01_42.png",
+                    "imagePath": "",
+                    "islandIcon": 0,
+                    "rtString": "",
+                    "seal": false,
+                    "temporary": 0,
+                    "town": 0,
+                    "trash": 0
+                }
+            }
+        },
+        "Element_002": {
+            "type": "SingleTextBox",
+            "value": "<FONT SIZE=\'12\'>아르카나 전용</FONT>"
+        },
+        "Element_003": {
+            "type": "SingleTextBox",
+            "value": "<FONT SIZE=\'12\'>귀속됨</FONT>"
+        },
+        "Element_004": {
+            "type": "MultiTextBox",
+            "value": "|<font color=\'#C24B46\'>거래 불가</font>"
+        },
+        "Element_005": {
+            "type": "ItemPartBox",
+            "value": {
+                "Element_000": "<FONT COLOR=\'#A9D0F5\'>기본 효과</FONT>",
+                "Element_001": "무기 공격력 +31392"
+            }
+        },
+        "Element_006": {
+            "type": "ItemPartBox",
+            "value": {
+                "Element_000": "<FONT COLOR=\'#A9D0F5\'>추가 효과</FONT>",
+                "Element_001": "추가 피해 +16.73%"
+            }
+        },
+        "Element_007": {
+            "type": "Progress",
+            "value": {
+                "forceValue": " ",
+                "maximum": 60480,
+                "minimum": 0,
+                "title": "<FONT SIZE=\'12\'><FONT COLOR=\'#A9D0F5\'>현재 단계 재련 경험치</FONT></FONT > ",
+                "value": 0,
+                "valueType": 1
+            }
+        },
+        "Element_008": {
+            "type": "ItemPartBox",
+            "value": {
+                "Element_000": " < FONT COLOR =\'#A9D0F5\'>세트 효과 레벨</FONT>",
+                "Element_001": "악몽 <FONT COLOR=\'#FFD200\'>Lv.1</FONT>"
+            }
+        },
+        "Element_009": {
+            "type": "IndentStringGroup", "value": {
+                "Element_000": {
+                    "contentStr": {
+                        "Element_000": {
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (타로카드)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        },
+                        "Element_001": { 
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (머리 방어구)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        },
+                        "Element_002": {
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (상의)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        },
+                        "Element_003": {
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (하의)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        },
+                        "Element_004": {
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (장갑)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        },
+                        "Element_005": {
+                            "bPoint": false,
+                            "contentStr": "<font size=\'12\'>악몽 (어깨 방어구)</font> [<FONT COLOR=\'#FFD200\'>Lv.1</FONT>]"
+                        }
+                    },
+                    "topStr": "<font size=\'14\'><font color=\'#91fe02\'>악몽</font></font>"
+                },
+                "Element_001": {
+                    "contentStr": {
+                        "Element_000": {
+                            "bPoint": true,
+                            "contentStr": "<font size=\'12\'><font color=\'#ffffff\'>마나 소모량이 <FONT COLOR=\'#99FF99\'>50%</FONT> 감소하고, 마나를 소모하는 스킬의 피해가 <FONT COLOR=\'#99FF99\'>12%</FONT> 증가한다.</font></font>"
+                        }
+                    },
+                    "topStr": "<font size=\'14\'><font color=\'#91fe02\'>2 세트 효과<BR>[<font size=\'14\'><font color=\'#aaaaaa\'><FONT COLOR=\'#FFD200\'>Lv.1</FONT> / Lv.2 / Lv.3</font></font>]</font></font>"
+                },
+                "Element_002": {
+                    "contentStr": {
+                        "Element_000": {
+                            "bPoint": true,
+                            "contentStr": "<font size=\'12\'><font color=\'#ffffff\'>마나를 보유하고 있는 클래스가 각성기 사용 시 \'마나 중독\' 효과를 획득한다.<BR>현재 마나량이 <FONT COLOR=\'#99FF99\'>30%</FONT> 이하에서 스킬 사용 시 \'마나 중독\' 효과가 제거 되고 \'끝없는 마나\' 효과로 변경된다.<BR>\'끝없는 마나\' 상태에서 현재 마나량이 <FONT COLOR=\'#99FF99\'>100%</FONT>가 될 시 해당 효과가 제거 되고 \'마나 중독\' 효과로 변경된다. 두 효과는 동시에 적용되지 않는다.<BR>\'마나 중독\' : 추가 피해가 <FONT COLOR=\'#99FF99\'>15%</FONT> 증가 하지만 마나를 소모하는 스킬 사용 시 최대 마나량의 <FONT COLOR =\'#ff9999\'>7%</FONT>가 추가로 소모된다.<BR>\'끝없는 마나\' : 매 초 마다 최대 마나량의 <FONT COLOR=\'#99FF99\'>3%</FONT>가 회복되며 스킬 재사용 대기시간이 <FONT COLOR=\'#99FF99\'>20%</FONT> 감소하고 공격 및 이동속도가<FONT COLOR =\'#99FF99\'>12%</FONT> 증가한다.</font></font>"
+                        }
+                    },
+                    "topStr": "<font size=\'14\'><font color=\'#91fe02\'>4 세트 효과<BR>[<font size=\'14\'><font color=\'#aaaaaa\'><FONT COLOR=\'#FFD200\'>Lv.1</FONT> / Lv.2 / Lv.3</font></font>]</font></font>"
+                },
+                "Element_003": {
+                    "contentStr": {
+                        "Element_000": {
+                            "bPoint": true,
+                            "contentStr": "<font size=\'12\'><font color=\'#ffffff\'>\'마나 중독\' 효과 시 적에게 주는 피해가 <FONT COLOR=\'#99FF99\'>15%</FONT> 추가 증가한다. <BR>\'끝없는 마나\' 효과 시 스킬 재사용 대기시간이 <FONT COLOR=\'#99FF99\'>15%</FONT> 추가 감소하고, 공격 및 이동속도가 <FONT COLOR=\'#99FF99\'>3%</FONT> 추가 증가한다.</font></font>"
+                        }
+                    },
+                    "topStr": "<font size=\'14\'><font color=\'#91fe02\'>6 세트 효과<BR>[<font size=\'14\'><font color=\'#aaaaaa\'><FONT COLOR=\'#FFD200\'>Lv.1</FONT> / Lv.2 / Lv.3</font></font>]</font></font>"
+                }
+            }
+        },
+        "Element_010": {
+            "type": "SingleTextBox",
+            "value": "<FONT SIZE=\'12\'><FONT COLOR=\'#C24B46\'>분해불가</FONT></FONT>"
+        },
+        "Element_011": {
+            "type": "SingleTextBox",
+            "value": "<Font color=\'#5FD3F1\'>[제작] 대도시 - 군단장 장비 제작</font>"
+        },
+        "Element_012": {
+            "type": "ShowMeTheMoney",
+            "value": "<FONT SIZE=\'12\'><FONT COLOR=\'#FFFFFF\'>내구도 <FONT COLOR=\'#FFFFFF\'>166/166</FONT ></FONT ></FONT >"
+        }
+    }   
+}
+document.body.style.backgroundColor = "lightblue";
+tooltip = data['Tooltip']
+document.getElementById('ItemTitle').innerHTML = tooltip.Element_000.value
+document.getElementById('ItemImg').src = tooltip.Element_001.value.slotData.iconPath
+document.getElementById('ItemCategory').innerHTML = tooltip.Element_001.value.leftStr0
+document.getElementById('ItemLevel').innerHTML = tooltip.Element_001.value.leftStr2
+document.getElementById('ItemQuality').innerHTML = tooltip.Element_001.value.leftStr1 + tooltip.Element_001.value.qualityValue
+document.getElementById('ItemAttack').innerHTML = tooltip.Element_005.value.Element_001
+document.getElementById('ItemAddEffect').innerHTML = tooltip.Element_006.value.Element_001
+document.getElementById('SetEffectTitle').innerHTML = tooltip.Element_008.value.Element_001
+document.getElementById('2SetEffectDetail').innerHTML = tooltip.Element_009.value.Element_001.topStr+tooltip.Element_009.value.Element_001.contentStr.Element_000.contentStr
+document.getElementById('4SetEffectDetail').innerHTML = tooltip.Element_009.value.Element_002.topStr+tooltip.Element_009.value.Element_002.contentStr.Element_000.contentStr
+document.getElementById('6SetEffectDetail').innerHTML = tooltip.Element_009.value.Element_003.topStr+tooltip.Element_009.value.Element_003.contentStr.Element_000.contentStr
